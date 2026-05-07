28de7c1fe2ff1e86f5516b92307f9d8eafbcbd67
Paulo Biao <paulobiao@gmail.com>
2026-05-07 19:41:28 -0400

    Add create user endpoint

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,14 +2,28 @@ exports.getUsers = (req, res) => {
   res.json({
     success: true,
     users: [
-      {
-        id: 1,
-        name: "Paulo"
-      },
-      {
-        id: 2,
-        name: "Cristiane"
-      }
+      { id: 1, name: "Paulo" },
+      { id: 2, name: "Cristiane" }
     ]
+  });
+};
+
+exports.createUser = (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({
+      success: false,
+      message: "O campo name é obrigatório"
+    });
+  }
+
+  res.status(201).json({
+    success: true,
+    message: "Usuário criado com sucesso",
+    user: {
+      id: 3,
+      name
+    }
   });
 };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {
   getUsers,
+  createUser
 } = require("../controllers/userController");
 
 router.get("/", getUsers);
+router.post("/", createUser);
 
 module.exports = router;
