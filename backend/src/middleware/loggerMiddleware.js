0909189467187d97c33e213bf12c0f0c7e613052
Paulo Biao <paulobiao@gmail.com>
2026-05-22 19:15:43 -0400

    Add structured logging with Pino

--- backend/src/middleware/loggerMiddleware.js
+++ backend/src/middleware/loggerMiddleware.js
@@ -1,9 +1,34 @@
-const morgan = require("morgan");
+const crypto = require("crypto");
+const pinoHttp = require("pino-http");
 
-morgan.token("request-id", (req) => req.requestId);
+const logger = require("../config/logger");
 
-const loggerMiddleware = morgan(
-  "[:request-id] :date[iso] :method :url :status :response-time ms - :remote-addr"
-);
+const loggerMiddleware = pinoHttp({
+  logger,
+
+  genReqId: () => {
+    return crypto.randomUUID();
+  },
+
+  customLogLevel: (_req, res, err) => {
+    if (res.statusCode >= 500 || err) {
+      return "error";
+    }
+
+    if (res.statusCode >= 400) {
+      return "warn";
+    }
+
+    return "info";
+  },
+
+  customSuccessMessage: (req, res) => {
+    return `${req.method} ${req.url} completed with ${res.statusCode}`;
+  },
+
+  customErrorMessage: (req, res) => {
+    return `${req.method} ${req.url} failed with ${res.statusCode}`;
+  },
+});
 
 module.exports = loggerMiddleware;
