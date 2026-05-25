c3b85ea20f851758e3f1d23e7dad9d5c3aa4acf7
Paulo Biao <paulobiao@gmail.com>
2026-05-25 07:14:25 -0400

    Add explicit AuthRequest typing

--- backend/src/types/auth.ts
+++ backend/src/types/auth.ts
@@ -1,9 +1,10 @@
 import type { Request } from "express";
+import type { UserRole } from "./roles";
 
 export interface JwtPayload {
   id: number;
   email: string;
-  role: string;
+  role: UserRole;
 }
 
 export interface AuthRequest extends Request {
